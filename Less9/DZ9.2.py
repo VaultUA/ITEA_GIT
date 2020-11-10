7f1281cc23a7cc038f7e7582fa3cf016b7a26152
VaultUA <chirkovskiy.vladimir@gmail.com>
2020-11-10 17:31:54 +0200

    Задача 9.2

--- Less9/DZ9.2.py
+++ Less9/DZ9.2.py
@@ -3,3 +3,46 @@
 поиска книги по какому-либо признаку (например, по автору или по году издания), добавления книг в библиотеку,
 удаления книг из нее, сортировки книг по разным полям.
 '''
+
+
+# Объявляем свой клас "библиотека"
+class library:
+
+    # Задаем свойства класа
+    def __init__(self, name, author, year, genre):
+        self.name = name
+        self.author = author
+        self.year = year
+        self.genre = genre
+
+
+def open_library():
+    if not library:
+        a = int(input("Книжная полка пустая\nХотите ее наполнить? (1 - Да, 2 - Нет)\n"))
+        if a == 2:
+            open_library()
+        elif a == 1:
+            name = input("Название:\n")
+            author = input("Автор:\n")
+            year = input("Год:\n")
+            genre = input("Жанр:\n")
+            add_book(name, author, year, genre)
+        else:
+            print("Ошибочный ввод")
+            open_library()
+    else:
+        print(library)
+
+
+def add_book(name, author, year, genre):
+    library.append(library(name=name,
+                            author=author,
+                            year=year,
+                            genre=genre))
+
+
+if __name__ == '__main__':
+library = library(\)
+
+open_library()
+print(library)
